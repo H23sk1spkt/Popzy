9c568e13723ee724d6ff77dd13c1867bc77c480a
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-06-28 02:06:19 +0700

    fix about scroll

--- Popzy.js
+++ Popzy.js
@@ -14,28 +14,48 @@ Popzy.elements=[]
 function Popzy(option= {}){
     // tạo ra 1 biến trong đó có các bên trái là gtri mặc định , bên phải là dữ liệu truyền vào , nếu có dữ liêu trùng thì sẽ đè lên dữ liệu con , và nếu ko có thì ko đè lên
     // this.opt  là thuộc tính của hàm ,còn const ... là biến cục bộ của hàm , chỉ hoạt động trong hàm tạo thôi
+    if(!option.content && !option.templateId){
+        return console.warn("cung cấp dữ liệu truyền vào")
+    }else if(option.content && option.templateId){
+        option.templateID=null
+    }else if(option.templateId){
+        this.template=$(`#${option.templateId}`)
+        if(!this.template){
+            return console.error(`#${option.templateId} does not exits`)
+    }
+    } else if(option.content){
+        this.content=option.content
+    }
+    // vì bản chất option là this.otp
     this.opt=Object.assign({
+        enablescroll: true, // yêu cầu mở khóa cuộn
         footer:false,
         destroyOnclose:true,
         closeModal: ["buttonModal","overlay","Escape"],
         cssElement:[],
+        ScrollLockTarget : () => {
+            return document.body 
+            // MẶC ĐỊNH TRẢ VÊ GIÁ TRỊ BODY , LÀ NƠI CHỨA TOÀN BỘ THÔNG TIN CỦA TRANG
+        }
         },option)
-    this.template=$(`#${this.opt.templateId}`)
-    if(!this.template){
-        return console.error(`#${this.opt.templateId} does not exits`)
-    }
+    
+    // kiểm tra xem nó có chưa các hình thức xóa trong khi đối tượng truyền vào ko
     const {closeModal}=this.opt
     this._allowButtonclose=closeModal.includes("buttonModal")
     this._allowoverlayclose=closeModal.includes("overlay")
     this._allowEscapeclose=closeModal.includes("Escape")
+    //khai báo biến
     this._footerButton= []
     this._handleEscapeKey=this._handleEscapeKey.bind(this)
 
 }
 //ko nền dùng arrow function nếu bên trong có this , vì this nó sẽ lấy từ phương thức modal nên phải tạo hàm function
     Popzy.prototype._build=function()  {
-        const content=this.template.content.cloneNode(true); // tạo ra 1 bảng sao 
-        //create element từng phần tử
+            const contentNode=this.content ? document.createElement("div") : this.template.content.cloneNode(true); // tạo ra 1 bảng sao 
+        if(this.content){
+            contentNode.innerHTML=this.content
+        }
+            //create element từng phần tử
         this.backup=document.createElement("div")// tạo sẵn 1 cái backup từ this
         this.backup.className="popzy__backup"
         
@@ -52,12 +72,12 @@ function Popzy(option= {}){
         container.append(close)
     }
         
-        const mcontent=document.createElement("div")
-        mcontent.className="popzy__content"
+        const contentModal=document.createElement("div")
+        contentModal.className="popzy__content"
         
         //append 
-        mcontent.append(content);
-        container.append(mcontent);
+        contentModal.append(contentNode);
+        container.append(contentModal);
         if(this.opt.footer){
             this._footerModal=document.createElement("div");
             this._footerModal.classList.add("popzy__footer")
@@ -101,75 +121,6 @@ function Popzy(option= {}){
             this._footerModal.innerHTML=this._footer
         }
     }
-
-    Popzy.prototype.open=function() {
-        Popzy.elements.push(this)
-        if(!this.backup){
-            this._build()
-        }
-        //attach
-        setTimeout(()=> {
-            this.backup.classList.add("popzy--show")
-        },0)// chạy bất đồng bộ nên settimeou sẽ chạy cuối cùng
-        this.ontransitionEnd(this.opt.onOpen)// đây là call back , sau khi callback được duyệt , nó sẽ duyệt hàm onOpen ở trong , tổng alf nó sẽ duyệt 2 lần
-        if(this._allowoverlayclose){
-        this.backup.onclick=(e)=> {
-            if(e.target===this.backup){
-                this.closeModal()
-            }}
-        }
-        if(this._allowEscapeclose){
-        document.addEventListener("keydown",this._handleEscapeKey )
-        }
-        document.body.style.overflow="hidden"
-        document.body.style.paddingRight=this._getscrollbar()+'px' // thêm padding để nó giữ ngueyen vị trí khi mât thanh cuộn
-        return this.backup
-    }
-        Popzy.prototype._handleEscapeKey= function(e)  {
-            // console.log(this); khi chưa dùng đin this nó sẽ trỏ vào documnet 
-            console.log(this);
-            
-
-            
-            const lastModal=Popzy.elements[Popzy.elements.length-1]
-            if (e.key === "Escape" && this===lastModal) {
-                this.closeModal();
-            }// do cái này  người dùng nhấn thì nó xóa hết các modal đang mở mà có thuộc tính "escape" nên xử lí rieng cái này
-
-}
-        Popzy.prototype.ontransitionEnd=function(callback) {
-            this.backup.ontransitionend=(e)=> {
-                if(e.propertyName!=="transform"){
-                    return
-                }
-                if(typeof callback==="function"){
-                    callback()
-                }
-            }
-        }
-        //tách hàm 
-        Popzy.prototype.closeModal=function(destroy=this.opt.destroyOnclose) {
-            Popzy.elements.pop()// để xóa phần tử cuối cùng
-            this.backup.classList.remove("popzy--show");
-            if(this._allowEscapeclose){
-                document.removeEventListener("keydown",this._handleEscapeKey )
-            }
-            this.ontransitionEnd(()=>{
-                    if(destroy){
-                    this.backup.remove()// xử lí sau khi hoàn thành hết toàn bộ css hieuj ứng thì mới được đống lại
-                    this.backup=null
-                    this._footerModal=null
-                    }
-                    if(typeof this.opt.onClose==="function"){
-                        this.opt.onClose()
-                    }
-                    if(!Popzy.elements.length){
-                    document.body.style.overflow=""
-                    document.body.style.paddingRight=""
-                    }
-        }
-        )// này nó cũng duyệt callback trước rồi mới tính thực thi thân hàm phía trong
-}
 Popzy.prototype._getscrollbar=function(){
         if(this._scroll) return this._scroll
         
@@ -185,6 +136,88 @@ Popzy.prototype._getscrollbar=function(){
         document.body.removeChild(div)// tránh đầy bộ nhớ , mình chỉ tìm kích thước thôi
         return this._scroll
 }// tạo hàm tính giá trị của thanh cuộn
+Popzy.prototype.open=function() {
+    Popzy.elements.push(this)
+    if(!this.backup){
+        this._build()
+    }
+    //attach
+    setTimeout(()=> {
+        this.backup.classList.add("popzy--show")
+    },0)// chạy bất đồng bộ nên settimeou sẽ chạy cuối cùng
+    this.ontransitionEnd(this.opt.onOpen)// đây là call back , sau khi callback được duyệt , nó sẽ duyệt hàm onOpen ở trong , tổng alf nó sẽ duyệt 2 lần
+    if(this._allowoverlayclose){
+    this.backup.onclick=(e)=> {
+        if(e.target===this.backup){
+            this.closeModal()
+        }}
+    }
+    if(this._allowEscapeclose){
+    document.addEventListener("keydown",this._handleEscapeKey )
+    }
+    if(this.opt.enablescroll){
+        const target=this.opt.ScrollLockTarget()
+        if(this._hasscrollbar(target)){
+            target.classList.add("popzy--no-scroll")
+            const pad=parseInt(getComputedStyle(target).paddingRight) // lấy tất cả các thuộc tính css của thẻ truyền vào
+            target.style.paddingRight=pad+this._getscrollbar()+'px'
+        }
+        
+    } // thêm padding để nó giữ ngueyen vị trí khi mât thanh cuộn
+    return this.backup
+}
+Popzy.prototype._handleEscapeKey= function(e)  {
+        // console.log(this); khi chưa dùng đin this nó sẽ trỏ vào documnet 
+        console.log(this);
+        
+
+        
+        const lastModal=Popzy.elements[Popzy.elements.length-1]
+        if (e.key === "Escape" && this===lastModal) {
+            this.closeModal();
+        }// do cái này  người dùng nhấn thì nó xóa hết các modal đang mở mà có thuộc tính "escape" nên xử lí rieng cái này
+
+}
+Popzy.prototype.ontransitionEnd=function(callback) {
+        this.backup.ontransitionend=(e)=> {
+            if(e.propertyName!=="transform"){
+                return
+            }
+            if(typeof callback==="function"){
+                callback()
+            }
+        }
+    }
+    //tách hàm 
+Popzy.prototype.closeModal=function(destroy=this.opt.destroyOnclose) {
+        Popzy.elements.pop()// để xóa phần tử cuối cùng
+        this.backup.classList.remove("popzy--show");
+        if(this._allowEscapeclose){
+            document.removeEventListener("keydown",this._handleEscapeKey )
+        }
+        this.ontransitionEnd(()=>{
+                if(destroy){
+                this.backup.remove()// xử lí sau khi hoàn thành hết toàn bộ css hieuj ứng thì mới được đống lại
+                this.backup=null
+                this._footerModal=null
+                }
+                if(typeof this.opt.onClose==="function"){
+                    this.opt.onClose()
+                }
+                if(this.opt.enablescroll && !Popzy.elements.length){
+                    const target=this.opt.ScrollLockTarget();
+                    console.log({target});
+                    
+                    target.classList.remove("popzy--no-scroll")
+                    target.style.paddingRight=""
+                }
+    }
+    )// này nó cũng duyệt callback trước rồi mới tính thực thi thân hàm phía trong
+}
+// XỬ LÍ TRƯỜNG HỢP NẾU NOOIN UDNG TRANG WEB KO ĐỦ ĐỂ TẠO KHÓA CUỘN MÀ NÓ VẪN PADDING RIGTH THÌ SAO
+Popzy.prototype._hasscrollbar= function(target){
+    return target.offsetWidth > target.clientWidth
+}
 
 
 
